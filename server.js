8afb77ff0ac146f940eddf474ab18786b9372422
aryangupta1325 <30853752+aryangupta1325@users.noreply.github.com>
2017-08-20 13:18:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,55 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articlecontent1 = {
+    title: 'article-one',
+    datef: '20-Sep-2017',
+    content: `test
+    test3
+    test5`
+};
+
+var articlecontent2 = {
+    title: 'article-two',
+    datef: '25-march-2017',
+    content: `Kritis Birthday
+    test3
+    test5`
+}
+
+function createTemplate(data){
+    var tiltle = data.title;
+    var datef = data.datef;
+    var content = data.content;
+        var htmlTemplate =
+        `
+            <html>
+            <head>
+            <title>
+            ${title}
+            </title>
+            </head>
+            <body>
+            ${datef}
+            <p>
+            ${content}
+            </p>
+            </body>
+            </html>`;
+    return htmlTemplate;        
+  
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articlecontent1));
+});
+
+app.get('/article-two', function (req, res) {
+  res.send(createTemplate(articlecontent2));
 });
 
 app.get('/ui/style.css', function (req, res) {
