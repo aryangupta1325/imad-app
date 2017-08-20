e0bf53296f3979a0b925a4cd7201d6b6fa022a21
aryangupta1325 <30853752+aryangupta1325@users.noreply.github.com>
2017-08-20 14:54:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
   res.send(createTemplate(articleName));
+  console.log(res);
 });
 
 app.get('/article-two', function (req, res) {
