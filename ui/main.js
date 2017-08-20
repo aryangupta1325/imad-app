c095c3cbd716013ebc01570fe001442162248853
aryangupta1325 <30853752+aryangupta1325@users.noreply.github.com>
2017-08-20 21:26:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,11 @@ var element = document.getElementById("maintext");
 element.innerHTML= "New value";
 
 var img = document.getElementById("modi");
+var marginLeft= 0;
+function moveRight(){
+    marginLeft = marginLeft +10;
+    img.style.marginLeft = marginLeft + "px";
+}
 img.onclick = function(){
-    img.style.marginLeft ="100px";
+    var interval = setInterval(moveRight,100);
 }
