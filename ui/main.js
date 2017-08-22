011f71632da46c442277d6e7c0867b051d24d996
aryangupta1325 <30853752+aryangupta1325@users.noreply.github.com>
2017-08-22 14:00:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,3 +11,9 @@ function moveRight(){
 img.onclick = function(){
     var interval = setInterval(moveRight,100);
 }
+var counter = 0;
+var ct1 = document.getElementById("ct1");
+ct1.onclick = function(){
+    counter = counter =1 ;
+
+}
